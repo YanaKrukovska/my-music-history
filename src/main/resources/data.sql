87c700bd704e1fc1da87079d443f9e6217d830e5
Yana Krukovska <jana.krua@gmail.com>
2019-07-13 14:12:15 +0300

    Updated entities: 1. Added constraints 2. Changed field type 3. Added joins
    Added test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,18 @@
 
-insert into BOOK values(1, 'Ulas Samchuck','Maria');
-insert into BOOK values(2, 'Orwell','1984');
-insert into song values (1, '25', 'adele', 'hello');
+insert into user (ID, GENDER,MAIL,NICK_NAME,PASSWORD,USER_NAME,BIRTH_DATE,COUNTRY_ID)
+values (1,'F', 'jana.krua@gmail.com', 'Yana', '', 'Yana', '2001-09-05',null);
+
+insert into user (ID, GENDER,MAIL,NICK_NAME,PASSWORD,USER_NAME,BIRTH_DATE,COUNTRY_ID)
+values (2,'M', 'v.krukovskyy@gmail.com', 'Slava', '', 'Slava', '1978-11-13',null);
+
+insert into song ( ID, ALBUM, ARTIST, TITLE)
+values (1, 'So Am I', 'Ava Max', 'So Am I');
+
+insert into song ( ID, ALBUM, ARTIST, TITLE)
+values (2, 'Sweet But Psycho', 'Ava Max', 'Sweet But Psycho');
+
+insert into listen ( ID, LISTEN_DATE, SONG_ID, USER_ID)
+values (1, '2019-07-13 14:03:00', 2, 2);
+
+insert into listen ( ID, LISTEN_DATE, SONG_ID, USER_ID)
+values (2, '2019-07-13 13:53:50', 2, 1);
