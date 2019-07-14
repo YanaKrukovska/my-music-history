d888b2bd0882a30af59629d3700b0440c144fc52
v.krukovskyi <v.krukovskyy@gmail.com>
2019-07-14 15:45:53 +0300

    - Fixed Id generation in Song entity - Introduced integration test properties -Fixed Song service test

--- src/test/java/com/ritacle/mhistory/service/SongServiceImplTest.java
+++ src/test/java/com/ritacle/mhistory/service/SongServiceImplTest.java
@@ -2,16 +2,17 @@ package com.ritacle.mhistory.service;
 
 import com.ritacle.mhistory.persistence.model.Song;
 import com.ritacle.mhistory.persistence.repository.SongRepository;
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-integration-test.properties")
 public class SongServiceImplTest {
     @Autowired
     SongService service;
@@ -20,11 +21,10 @@ public class SongServiceImplTest {
     SongRepository repository;
 
     @Test
-    @Ignore
     public void save() {
         Song song = new Song("My Way", "Ava Max", "My Way");
-        repository.findAll();
-        service.save(song);
+        Song savedSong = service.save(song);
+        Assert.assertNotNull(savedSong.getId());
     }
 
 }
---
+++ src/test/resources/integration-test-data.sql
@@ -0,0 +1,21 @@
+
+insert into user (ID, GENDER,MAIL,NICK_NAME,PASSWORD,USER_NAME,BIRTH_DATE,COUNTRY_ID)
+values (1,'F', 'jana.krua@gmail.com', 'Yana', '', 'Yana', '2001-09-05',null);
+
+insert into user (ID, GENDER,MAIL,NICK_NAME,PASSWORD,USER_NAME,BIRTH_DATE,COUNTRY_ID)
+values (2,'M', 'v.krukovskyy@gmail.com', 'Slava', '', 'Slava', '1978-11-13',null);
+
+insert into song ( ID, ALBUM, ARTIST, TITLE)
+values (1, 'So Am I', 'Ava Max', 'So Am I');
+
+insert into song ( ID, ALBUM, ARTIST, TITLE)
+values (2, 'Sweet But Psycho', 'Ava Max', 'Sweet But Psycho');
+
+insert into song ( ID, ALBUM, ARTIST, TITLE)
+values (3, 'Long Way Down', 'Tom Odell', 'I Know');
+
+insert into listen ( ID, LISTEN_DATE, SONG_ID, USER_ID)
+values (1, '2019-07-13 14:03:00', 2, 2);
+
+insert into listen ( ID, LISTEN_DATE, SONG_ID, USER_ID)
+values (2, '2019-07-13 13:53:50', 2, 1);
