4cfed8830d0c44ce47a162454668b544e15af767
Yana Krukovska <jana.krua@gmail.com>
2019-08-07 20:41:10 +0300

    - Renamed entity and services

--- src/main/java/com/ritacle/mhistory/service/StatisticsServiceImpl.java
+++ src/main/java/com/ritacle/mhistory/service/StatisticsServiceImpl.java
@@ -1,6 +1,6 @@
 package com.ritacle.mhistory.service;
 
-import com.ritacle.mhistory.persistence.model.stats.SongStats;
+import com.ritacle.mhistory.persistence.model.stats.LastListen;
 import com.ritacle.mhistory.persistence.repository.SongStatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -17,7 +17,7 @@ public class StatisticsServiceImpl implements StatisticsService {
 
 
     @Override
-    public List<SongStats> getUserListens(String userMail, Sort sort) {
+    public List<LastListen> getLastUserListens(String userMail, Sort sort) {
 
         return repository.findFirst30ByUserMailIgnoreCase(userMail, sort);
     }
