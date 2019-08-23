2d8033529e614fe0778bbb027677bb6b9e49edca
Viacheslav Krukovskyi <v.krukovskyy@gmail.com>
2019-08-23 16:21:21 +0300

    Revert "Top feature"

--- src/main/java/com/ritacle/mhistory/rest/ReportRestService.java
+++ src/main/java/com/ritacle/mhistory/rest/ReportRestService.java
@@ -1,7 +1,6 @@
 package com.ritacle.mhistory.rest;
 
-import com.ritacle.mhistory.persistence.model.LastListen;
-import com.ritacle.mhistory.persistence.model.ListenAmount;
+import com.ritacle.mhistory.persistence.model.stats.LastListen;
 import com.ritacle.mhistory.service.StatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
@@ -10,24 +9,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/report")
 public class ReportRestService {
 
-    @Autowired
-    private StatisticsService statisticsService;
+@Autowired
+private StatisticsService statisticsService;
 
 
-    @GetMapping("/listen/last/{mail}")
-    public List<LastListen> getLastListens(@PathVariable String mail) {
-        return statisticsService.getLastUserListens(mail, Sort.by(Sort.Order.desc("listenDate")));
-    }
-
-    @GetMapping("/top/songs/{mail}/{startDate}/{endDate}")
-    public List<ListenAmount> getUserTopListens(@PathVariable String mail, @PathVariable Date startDate, @PathVariable Date endDate) {
-        return statisticsService.getUserTopListens(mail, startDate, endDate);
-    }
+   @GetMapping("/listen/last/{mail}")
+   public List<LastListen> getLastListens(@PathVariable String mail) {
+       return statisticsService.getLastUserListens(mail, Sort.by(Sort.Order.desc("listenDate")));
+  }
 }
--- src/main/java/com/ritacle/mhistory/service/StatisticsService.java
+++ src/main/java/com/ritacle/mhistory/service/StatisticsService.java
@@ -1,17 +1,13 @@
 package com.ritacle.mhistory.service;
 
-import com.ritacle.mhistory.persistence.model.LastListen;
-import com.ritacle.mhistory.persistence.model.ListenAmount;
+import com.ritacle.mhistory.persistence.model.stats.LastListen;
 import org.springframework.data.domain.Sort;
 
-import java.util.Date;
 import java.util.List;
 
 public interface StatisticsService {
 
-    List<LastListen> getLastUserListens(String userMail, Sort sort);
-
-    List<ListenAmount> getUserTopListens(String userMail, Date startDate, Date endDate);
 
 
+  List<LastListen> getLastUserListens(String userMail, Sort sort);
 }
