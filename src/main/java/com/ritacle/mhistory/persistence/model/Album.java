c68656b3578d0f12baacba8b3aaae8647d341d2e
Yana Krukovska <jana.krua@gmail.com>
2019-09-02 18:58:30 +0300

    - Added json serialization annotation

--- src/main/java/com/ritacle/mhistory/persistence/model/Album.java
+++ src/main/java/com/ritacle/mhistory/persistence/model/Album.java
@@ -1,8 +1,11 @@
 package com.ritacle.mhistory.persistence.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.util.Date;
 
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
 public class Album {
 
